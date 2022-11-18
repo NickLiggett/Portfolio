078aeb81b3ad9447baa0d7bf2d47d849b62fbda2
Nick Liggett <nick.liggett@gmail.com>
2022-11-18 14:44:00 -0700

    Attempt fix at Resume bug

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,10 +1,13 @@
-import "./Resume.css"
-import resume from "../../images/Nicholas Liggett Resume.pdf"
+import "./Resume.css";
+import resume from "../../images/Nicholas Liggett Resume.pdf";
 
 const Resume = () => {
-    return (
-            <embed className="resume" src={resume}></embed>
-    )
-}
+  return (
+    <embed
+      className="resume"
+      src="https://github.com/NickLiggett/Portfolio/blob/4294255f2e5e497998dea26edb3b5d80d49cb06f/src/images/Nicholas%20Liggett%20Resume.pdf"
+    ></embed>
+  );
+};
 
-export default Resume
+export default Resume;
