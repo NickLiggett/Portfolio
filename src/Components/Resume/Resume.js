54abc3a502cbada1bcd00964fe28896f65b67a03
Nick Liggett <nick.liggett@gmail.com>
2022-11-18 16:41:36 -0700

    Attempt Resume Fix

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
   return (
     <embed
       className="resume"
-      src="https://github.com/NickLiggett/Portfolio/blob/4294255f2e5e497998dea26edb3b5d80d49cb06f/src/images/Nicholas%20Liggett%20Resume.pdf"
+      src={resume}
     ></embed>
   );
 };
