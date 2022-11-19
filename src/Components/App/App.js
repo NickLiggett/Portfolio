14a4c22c347b0c7d9c42efb7d86b6d9a777466eb
Nick Liggett <nick.liggett@gmail.com>
2022-11-18 17:07:33 -0700

    Add Router Switch to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,22 +4,18 @@ import Nav from "../Nav/Nav";
 import AboutMe from "../AboutMe/AboutMe";
 import Resume from "../Resume/Resume";
 import Projects from "../Projects/Projects";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <main>
       <Header />
       <Nav />
-      <Route exact path="/Portfolio">
-        <AboutMe />
-      </Route>
-      <Route exact path="/Portfolio/resume">
-        <Resume />
-      </Route>
-      <Route exact path="/Portfolio/projects">
-        <Projects />
-      </Route>
+      <Switch>
+        <Route exact path="/Portfolio" component={AboutMe}></Route>
+        <Route exact path="/Portfolio/resume" component={Resume}></Route>
+        <Route exact path="/Portfolio/projects" component={Projects}></Route>
+      </Switch>
     </main>
   );
 }
